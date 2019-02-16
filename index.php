393061c6e4b4d591a740dfbc64b1b364d7df800e
wofoon <47618082+wofoon@users.noreply.github.com>
2019-02-16 14:08:06 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,65 @@
+<?php
+   $get=$_REQUEST["code"];
+   $array = array(
+   "250536", //无欢
+   "0EI44R", //无欢
+   "SMYD4S", //低调
+   "Q6Y2W5"//橛子
+   );
+   if(!in_array($get,$array)){
+			echo "<img src='./rmm.jpg' width='276' height='220'>";
+			exit;
+	   }
+?>
+<!DOCTYPE html>
+<html lang="en">
+ <head> 
+  <meta charset="utf-8" /> 
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimum-scale=1.0, maximum-scale=1.0" /> 
+  <title>粉象生活</title> 
+  <meta name="apple-mobile-web-app-capable" content="yes" /> 
+  <meta name="apple-mobile-web-app-status-bar-style" content="black" /> 
+  <meta name="apple-touch-fullscreen" content="yes" /> 
+  <meta name="format-detection" content="telephone=no, email=no" /> 
+  <meta http-equiv="Expires" content="0" /> 
+  <meta http-equiv="Pragma" content="no-cache" /> 
+  <meta http-equiv="Cache-control" content="no-cache" /> 
+  <style>*{margin:0;padding:0;box-sizing:border-box}body{background:#fff5e6;text-align:center}a{text-decoration:none;color:#fff;border:none;outline:none;-webkit-tap-highlight-color:transparent}a:active{color:hsla(0,0%,100%,.5)}img{border:none;vertical-align:middle}header{overflow:hidden;margin-bottom:16px}header img{width:100%}.video-wrap iframe{width:16rem;height:9rem;margin:0 auto}.cont{padding:2rem 2.4rem 0}.cont .code-wrap{display:flex;height:2.4rem;line-height:2.4rem;overflow:hidden;border:1px solid #ffa819;border-radius:1.2rem;font-size:.8rem}.cont .code-wrap span{width:8.6rem;color:#ffa819;flex:auto}.cont .code-wrap a{width:5.4rem;background:#ffa819;flex:none}.cont .tip{margin:.4rem 0 .8rem;line-height:.85rem;font-size:.6rem;color:#666}.cont .btn-wrap a{display:block;height:2.4rem;line-height:2.4rem;background:#ffa819;border-radius:1.2rem;box-shadow:0 4px 6px 0 rgba(245,166,35,.4);font-size:.9rem}footer{position:relative;width:100%;margin:3rem 0 1rem}footer img{height:2.3rem}.fixed{opacity:0;position:fixed;top:40%;left:50%;transition:all .3s;transform:translateX(-50%);padding:8px 20px;color:#fff;background:rgba(0,0,0,.8);border-radius:5px;font-size:15px;line-height:17px}</style> 
+ </head> 
+ <body> 
+  <header> 
+   <img src="img_header.png" /> 
+  </header> 
+  <section class="video-wrap"> 
+   <iframe src="https://v.qq.com/iframe/player.html?vid=e0744qg4xa9" allowfullscreen="" frameborder="0" classname="video"> </iframe> 
+  </section> 
+  <section class="cont"> 
+<div class="code-wrap"> 
+<span id="code">邀҉请҉码҉：</span> 
+<a id="copy-btn">复制邀҉请҉码҉</a>
+</div>
+   <span class="fixed">已复制到剪贴板</span> 
+   <div class="tip">
+	复制邀҉请҉码҉，下载APP后，点「我的」微信一键登录<br>
+	<span style="color:#FF0000">注：非注册用户购҉物҉只能搜索优҉惠҉券҉，没有现金返利</span>
+   </div> 
+   <div class="btn-wrap"> 
+    <a href="http://a.app.qq.com/o/simple.jsp?pkgname=com.n_add.android">下载APP&gt;&gt;</a> 
+   </div> 
+  </section> 
+  <footer> 
+   <img src="img_logo.png" /> 
+  </footer> 
+  <script src="invite-download.js"></script>  
+  <script>
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?2df59ff14827e4baf12d6bb0a2394fb2";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
+</script>
+
+ </body>
+</html>
